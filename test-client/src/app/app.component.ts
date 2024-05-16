7cbfdbc3d00dc4b665f38cd8d93969cce2c03bf2
buhleqampi <buhle3857@gmail.com>
2024-05-16 09:54:01 +0200

    appcomponent

--- test-client/src/app/app.component.ts
+++ test-client/src/app/app.component.ts
@@ -1,35 +1,10 @@
 import { Component } from '@angular/core';
-import { MasterService } from './master.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class AuthComponent {
-
-  constructor(private authService: AuthService) { }
-
-  registerUser(userData: any) {
-    this.authService.register(userData).subscribe(
-      response => {
-        console.log('User registered successfully', response);
-      },
-      error => {
-        console.error('Error registering user', error);
-      }
-    );
-  }
-
-  signInUser(userData: any) {
-    this.authService.signIn(userData).subscribe(
-      response => {
-        console.log('User signed in successfully', response);
-      },
-      error => {
-        console.error('Error signing in user', error);
-      }
-    );
-  }
+export class AppComponent {
+  title = 'test-client';
 }
-
