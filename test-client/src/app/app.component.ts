338cc3b21ed79b924ae7568c43ae8c58f96c331f
JaySeteni <jay.seteni@gmail.com>
2024-05-16 09:49:57 +0200

    pushing

--- test-client/src/app/app.component.ts
+++ test-client/src/app/app.component.ts
@@ -2,34 +2,11 @@ import { Component } from '@angular/core';
 import { MasterService } from './master.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AuthComponent {
+export class AppComponent {
 
-  constructor(private authService: AuthService) { }
-
-  registerUser(userData: any) {
-    this.authService.register(userData).subscribe(
-      response => {
-        console.log('User registered successfully', response);
-      },
-      error => {
-        console.error('Error registering user', error);
-      }
-    );
-  }
-
-  signInUser(userData: any) {
-    this.authService.signIn(userData).subscribe(
-      response => {
-        console.log('User signed in successfully', response);
-      },
-      error => {
-        console.error('Error signing in user', error);
-      }
-    );
-  }
 }
 
