0f65dc943c396a7ff872f822f673cc724a9201d7
JaySeteni <jay.seteni@gmail.com>
2024-05-16 14:01:23 +0200

    push updates

--- test-client/src/app/comps/signup/signup.component.ts
+++ test-client/src/app/comps/signup/signup.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
 
+  signupForm: FormGroup | any;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.maxLength(10)]]
+    })
+  }
+
+  onSubmit() {
+    return console.log(this.signupForm.value);
+
+  }
 }
