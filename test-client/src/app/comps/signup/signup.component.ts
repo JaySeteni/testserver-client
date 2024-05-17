c0a11bf0772153e4a25b0eea566b46ab5db4aba3
JaySeteni <jay.seteni@gmail.com>
2024-05-17 13:24:22 +0200

    push

--- test-client/src/app/comps/signup/signup.component.ts
+++ test-client/src/app/comps/signup/signup.component.ts
@@ -1,26 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../Service/auth-token.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent implements OnInit{
+export class SignupComponent{
 
-  signupForm: FormGroup | any;
+  submitted = false;
+  showPassword = false;
 
-  constructor(private fb: FormBuilder) {}
+  signupForm = this.fb.group({
 
-  ngOnInit(): void {
-    this.signupForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.maxLength(10)]]
+  email: ['', [Validators.required, Validators.email]],
+  password: ['',[Validators.required]]
+})
+
+  constructor (private fb: FormBuilder,private auth:AuthService) {}
+
+  onSubmit(): void {
+    console.log('submitted successfully', this.signupForm.value,
+      this.signupForm.invalid
+    )
+
+    this.auth.register( this.signupForm.value).subscribe((data)=>{
+      console.log(data)
     })
   }
 
-  onSubmit() {
-    return console.log(this.signupForm.value);
-
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
   }
 }
